1a08a27a2e1ca08bcd44f2849dbe4e8cd183f1e5
zodicto <minhtan210703@gmail.com>
2025-02-16 18:23:34 +0700

    hihi

---
+++ SE-Game/Assets/BulletController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 10f; // Tốc độ bay của đạn
+    public float lifetime = 2f; // Thời gian tồn tại của đạn
+
+    void Start()
+    {
+        // Hủy đạn sau một khoảng thời gian
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        // Di chuyển đạn theo hướng lên
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+}
---
+++ SE-Game/Assets/SpaceshipController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Camera gameCamera;
+
+    void Start()
+    {
+        gameCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        // Lấy input từ bàn phím
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // Di chuyển phi thuyền
+        transform.Translate(new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime);
+
+        ClampPosition();
+    }
+
+    void ClampPosition()
+    {
+        // Lấy vị trí phi thuyền
+        Vector3 pos = transform.position;
+
+        // Tính toán biên của Camera (trái dưới và phải trên)
+        Vector3 bottomLeft = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, Mathf.Abs(gameCamera.transform.position.z - transform.position.z)));
+        Vector3 topRight = gameCamera.ViewportToWorldPoint(new Vector3(1, 1, Mathf.Abs(gameCamera.transform.position.z - transform.position.z)));
+
+        // Lấy kích thước của phi thuyền để bù trừ
+        float shipWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        float shipHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+
+        // Giới hạn vị trí x và 
+        pos.x = Mathf.Clamp(pos.x, bottomLeft.x + shipWidth, topRight.x - shipWidth);
+        pos.y = Mathf.Clamp(pos.y, bottomLeft.y - (float) 0.5 * shipHeight, topRight.y - (float)1.5 * shipHeight);
+
+        // Gán lại vị trí giới hạn
+        transform.position = pos;
+    }
+}
